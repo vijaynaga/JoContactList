05722a5d4e9349481cc0384552562777fe7890c0
Vijayanathan Naganathan <vijay@triplepulse.in>
2016-07-30 15:43:16 +0530

    commit 5

--- handlers/contacts.js
+++ handlers/contacts.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var repository = require('../lib/contactRepository');
+var repository = require('../lib/api/contactRepository');
 
 module.exports = {
     get: function contacts_get(req, res) {
